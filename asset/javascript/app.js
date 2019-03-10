946cf32a0a6d92eb2005bcaba0dae1e3426bcf7e
avlegend <lightsprogrammer@gmail.com>
2019-03-10 13:09:14 -0700

    updated firebase and time

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -7,6 +7,8 @@ var config = {
   storageBucket: "train-scheduler-cf922.appspot.com",
   messagingSenderId: "292519811756"
 };
+
+
 firebase.initializeApp(config);
 var database = firebase.database();
 
@@ -29,12 +31,13 @@ $("#add-train").on("click", function (event) {
       startTime: trainStartTime,
       frequency: trainFrequency
     };
+    
 
     // Uploads employee data to the database
     database.ref().push(newTrain);
 
-    console.log(name.name);
-    console.log(destination.trainDestination)
+    console.log(newTrain.name);
+    console.log(newTrain.destination)
   
    // alert("added successfully")
 
@@ -46,25 +49,56 @@ $("#add-train").on("click", function (event) {
 });
 
 //Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function (childsnapshot) {
-  console.log(childsnapshot.val());
+database.ref().on("child_added", function (snapshot) {
+  console.log(snapshot.val());
   //var monthsWorked = monthsElapsed(snapshot.val().startDate);  //Calculate months worked using current date
   
   // Store everything into a variable.
-  var name = childSnapshot.val().name;
-  var trainDestination = childSnapshot.val().destination;
-  var trainStartTime = childSnapshot.val().startTime;
-  var trainFrequency = childSnapshot.val().frequency;
+  var name = snapshot.val().name;
+  var trainDestination = snapshot.val().destination;
+  var trainStartTime = snapshot.val().startTime;
+  var trainFrequency = snapshot.val().frequency;
    console.log(name)
+   // the moment starts here
+   // we "split" the time into hours and mins
+   var startTimeArray = trainStartTime.split(':');
+  var hours = startTimeArray[0];
+  var mins = startTimeArray[1];
+  var trainTime = moment().hours(hours).minutes(mins);
+  console.log(trainTime.format("hh:mm A"));
+  var maxMoment = moment.max(moment(), trainTime);
 
+  var nextTrainArrival;
+  var minutesAway;
 
+  if(maxMoment === trainTime) {
+    //the trains start time is already  the future and we can asume the start time is the next arrival
+    //and the difference between the start time and the current moment is our minutes away
+    nextTrainArrival = trainTime.format("hh:mm A");
+    minutesAway = trainTime.diff(moment(), 'minutes');
+  }
+  else {
+    //we need to do some math to figure out when the net arrival is and how many minutes away that is
+    console.log('We hit the else nad nothing should be working becasue we havent written this code')
+    //
+    var minutesSinceTrainLeft = moment().diff(trainTime, 'minutes');
+    var minutesRemaining = minutesSinceTrainLeft % trainFrequency;
+    minutesAway = trainFrequency - minutesRemaining;
+    nextTrainArrival = moment().add(minutesAway,'m').format("hh:mm A");
+    
+
+  }
+  
   var row = $("<tr>");
-  row.append($("<th>").text(snapshot.val().name));
-  //row.append($("<th>").text(snapshot.val().role));
-  row.append($("<th>").text(snapshot.val().destination));
-  //row.append($("<th>").text(monthsWorked));
-  row.append($("<th>").text(snapshot.val().startTime));
-  //row.append($("<th>").text(monthsWorked * parseInt(snapshot.val().rate)));
+  row.append($("<th>").text(name));
+  row.append($("<th>").text(trainDestination));
+  row.append($("<th>").text(trainStartTime));
+  row.append($("<th>").text(trainFrequency));
+  row.append($("<th>").text(nextTrainArrival));
+  row.append($("<th>").text(minutesAway));
+  // min next arrival
+  
+  // min away
 
   $("#train-data").append(row);
 });
